2d874e972a67b71cd6179ca28a5a35c5e3082f43
Colin Campbell-McPherson <colin@logaan.net>
2012-01-16 20:03:46 +1100

    clean rails 3.11 generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsTemplate2::Application.config.secret_token = '64714875d6318373f6b8a78692655ab1f479403e1dfc9047c09a683c5bc3f53417a3667273cbc92e566051a5c124a24d9bd6e365b663209cbe41af6a27092da5'
