bbc69479c9b3bc5dcfbb05c7af59c2d2ddc788ab
Colin Campbell-McPherson <colin@logaan.net>
2012-01-16 21:18:09 +1100

    configured admins area

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,3 +1,15 @@
 class Admin::AdminsController < BackEndController
-  active_scaffold :admin
+  active_scaffold :admin do |config|
+    config.columns.add [:email, :password, :password_confirmation]
+
+    config.columns[:password].form_ui = :password
+    config.columns[:password_confirmation].form_ui = :password
+
+    config.list.columns   = [:email, :created_at]
+
+    config.show.columns.remove [:password, :password_confirmation, :encrypted_password]
+
+    config.create.columns = [:email, :password, :password_confirmation]
+    config.update.columns = [:email, :password, :password_confirmation]
+  end
 end
